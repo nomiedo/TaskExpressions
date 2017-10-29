be07ca71a891e1f9a0813cc9af0d8e7ad2001fee
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-10-29 22:22:54 +0300

    add all projects

---
+++ ExtensionMapper/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExtensionMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
+
+            // var res = mapper.Map(new Foo());
+            var res = mapper.Map(new Foo());
+            Console.Read();
+        }
+    }
+
+    public class Mapper<TSource, TDestination>
+    {
+        Func<TSource, TDestination> mapFunction;
+        internal Mapper(Func<TSource, TDestination> func)
+        {
+            mapFunction = func;
+        }
+        public TDestination Map(TSource source)
+        {
+            return mapFunction(source);
+        }
+    }
+
+    public class MappingGenerator
+    {
+        public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
+        {
+            var source = Expression.Parameter(typeof(TSource), "source");
+            var body = Expression.MemberInit(Expression.New(typeof(TDestination)),
+                source.Type.GetProperties().Select(p => Expression.Bind(typeof(TDestination).GetProperty(p.Name), Expression.Property(source, p))));
+            var expr = Expression.Lambda<Mapper<TSource, TDestination>>(body, source);
+            return expr.Compile();
+        }
+    }
+    public class Foo {
+
+    }
+    public class Bar {
+
+    }
+
+}
--- TaskExpression1/Program.cs
+++ TaskExpression1/Program.cs
@@ -1,49 +1,63 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace TaskExpression1
 {
+
+        public static class MappingGenerator
+    {
+            public static Func<TInput, TOutput> CreateMapFunc<TInput, TOutput>()
+            {
+                var source = Expression.Parameter(typeof(TInput), "source");
+                var body = Expression.MemberInit(Expression.New(typeof(TOutput)),
+                    source.Type.GetProperties().Select(p => Expression.Bind(typeof(TOutput).GetProperty(p.Name), Expression.Property(source, p))));
+                var expr = Expression.Lambda<Func<TInput, TOutput>>(body, source);
+                return expr.Compile();
+            }
+        }
+        public static class MapFunc<TInput, TOutput>
+        {
+            public static readonly Func<TInput, TOutput> Instance = MappingGenerator.CreateMapFunc<TInput, TOutput>();
+        }
+        public struct Unit<T>
+        {
+            public readonly T Value;
+            public Unit(T value)
+            { Value = value; }
+
+            public U MapTo<U>()
+            {
+                return MapFunc<T, U>.Instance(Value);
+            }
+        }
+        public static class Extensions
+        {
+            public static Unit<T> Unit<T>(this T source)
+            {
+                return new Unit<T>(source);
+            }
+        }
+        // Test
+        public class Foo
+        {
+            public string Bar { get; set; }
+            public string Baz { get; set; }
+        }
+
+    public class Foo2
+    {
+        public string Bar { get; set; }
+        public string Baz { get; set; }
+    }
+
     class Program
-    {
-        static void Main(string[] args)
         {
-            var mapGenerator = new MappingGenerator();
-            var mapper = mapGenerator.Generate<Foo, Bar>();
-
-            var res = mapper.Map(new Foo());
+            static void Main(string[] args)
+            {
+                Foo2 obj = new Foo2 { Bar = "My Name", Baz = "Some other data" };
+                //var foo = new Foo { Bar = obj.Bar, Baz = obj.Baz };
+                var foo = obj.Unit().MapTo<Foo>();
+            }
         }
-    }
-
-    public class Mapper<TSource, TDestination>
-    {
-        Func<TSource, TDestination> mapFunction;
-        internal Mapper(Func<TSource, TDestination> func)
-        {
-            mapFunction = func;
-        }
-        public TDestination Map(TSource source)
-        {
-            return mapFunction(source);
-        }
-    }
-
-    public class MappingGenerator
-    {
-        public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
-        {
-
-        }
-    }
-    public class Foo
-    {
-        public string Word { get; set; }
-
-        public int Number { get; set; }
-    }
-    public class Bar
-    {
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-    }
-
 }
